b148d5cff0c3fe580c15ef909ea7b1cacfd79df8
MikiProvince <54849210+MikiProvince@users.noreply.github.com>
2022-09-08 15:30:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.fromTo(".hero h2", { opacity:0 }, { opacity: 1, duration: 1, delay: 2 });
+gsap.fromTo(
+    ".hero h3",
+    { opacity:0 },
+    { opacity: 1, duration: 1, delay: 2.2 }
+);
+gsap.fromTo(
+    ".menu #one",
+    { opacity:0 },
+    { opacity: 1, duration: 1, delay: 2.8 }
+);
+gsap.fromTo(
+    ".menu #two",
+    { opacity:0 },
+    { opacity: 1, duration: 1, delay: 3 }
+);
+gsap.fromTo(
+    ".menu #three",
+    { opacity:0 },
+    { opacity: 1, duration: 1, delay: 3.2 }
+);
